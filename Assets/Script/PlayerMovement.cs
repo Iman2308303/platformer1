545bb29edc0939f0eef138ca5fe28a5a3a352cdb
Iman2308303 <2308303@student.toa.edu.my>
2024-03-05 12:30:37 +0800

    coyote time and buffer time/jump

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : Movement
 {
-    public Cooldown Random;
     protected override void HandleInput()
     {
         _InputDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-        if (Input.GetButton("Jump")
-            && Random.CurrentProgress != Cooldown.Progress.Ready
-                || Random.CurrentProgress != Cooldown.Progress.Finished)
+        if (Input.GetButton("Jump"))
         {
+            DoJump();
             _IsJumping = true;
         }
         else
         {
             _IsJumping = false;
-         }
+        }
     }
+
+
   
 }
--- Assets/Script/movement.cs
+++ Assets/Script/movement.cs
@@ -1,5 +1,6 @@
  using System.Collections;
 using System.Collections.Generic;
+using Unity.Profiling;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
@@ -7,20 +8,30 @@ public class Movement : MonoBehaviour
     public float Acceleration = 10f;
     public float JumpForce = 50f;
 
+    //ground check
     public Transform GroundCheck;
     public float GroundCheckRadius = 1f;
     public float MaxSlopeAngle = 45f;
+
+    public Cooldown CoyoteTime;
+    public Cooldown BufferTime;
+
+    
 
     public LayerMask GroundLayerMask;
 
     protected bool _IsGrounded = false;
     protected bool _IsJumping = false;
+    protected bool _canJump = true;
+
 
     protected Vector3 _InputDirection;
 
     protected Rigidbody2D _rigidbody2d;
     protected Collider2D _collider2d;
     // Start is called before the first frame update
+
+
     void Start()
     {
         _rigidbody2d = GetComponent<Rigidbody2D>();
@@ -31,6 +42,7 @@ public class Movement : MonoBehaviour
     void Update()
     {
         HandleInput();
+        
     }
 
     protected virtual void FixedUpdate()
@@ -40,13 +52,42 @@ public class Movement : MonoBehaviour
         HandleMovement();
     }
 
+    protected void TryBufferJump()
+    {
+        
+    }
+    //my assignment
+    protected void GetBufferjump()
+    {
+        if (_rigidbody2d.velocity.y < 0 && _IsGrounded)
+        {
+            _IsJumping = false;
+            Debug.Log("working");
+            DoJump();
+            
+        }
+
+      
+    }
+
     protected virtual void HandleInput()
     {
         
     }
     protected virtual void DoJump()
     {
+        if (!_canJump)
+            return;
+
+        if (CoyoteTime.CurrentProgress == Cooldown.Progress.Finished)
+            return;
+
+        _canJump = false;
+        _IsJumping = true;
+
         _rigidbody2d.velocity = new Vector2(_rigidbody2d.velocity.x, JumpForce);
+
+        CoyoteTime.StopCooldown();
     }
 
     void HandleMovement()
@@ -69,10 +110,26 @@ public class Movement : MonoBehaviour
         _IsGrounded = Physics2D.OverlapCircle(GroundCheck.position, GroundCheckRadius, GroundLayerMask);
         Debug.Log(_IsGrounded);
 
-        if ( _IsGrounded )
+        if (_rigidbody2d.velocity.y <= 0)
         {
+            _IsJumping = false;
+        }
+
+        if ( _IsGrounded && !_IsJumping )
+        {
+            _canJump = true;
+
+            if (CoyoteTime.CurrentProgress != Cooldown.Progress.Ready)
+                CoyoteTime.StopCooldown();
+
+          
+           
             if ( _IsJumping )
             DoJump();
         }
+        if (!_IsGrounded && _IsJumping && CoyoteTime.CurrentProgress == Cooldown.Progress.Ready)
+            CoyoteTime.StopCooldown();
+
+
     }
 }
